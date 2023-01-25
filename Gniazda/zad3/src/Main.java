50d132c9362d47266e53f7778e19c080658931a6
Justyna Krotoszyńska <justyna.krotoszynska@gmail.com>
2023-01-25 21:51:03 +0100

    zadania

--- Gniazda/zad3/src/Main.java
+++ Gniazda/zad3/src/Main.java
@@ -1,2 +1,23 @@
-public class Main {
+import java.net.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+        if (args.length < 1){
+            System.out.println("Wywołanie programu: Main nazwa_hosta");
+            System.exit(0);
+        }
+        String host = args[0];
+        InetAddress inetAddress = null;
+        try{
+            inetAddress = InetAddress.getByName(host);
+        }
+        catch(UnknownHostException e){
+            System.out.println(
+                    "Nie można uzyskać adresu IP dla hosta " + host);
+            System.exit(0);
+        }
+        String ip = inetAddress.getHostAddress();
+        System.out.println("Adres IP komputera " + host +" to: " + ip);
+    }
 }
